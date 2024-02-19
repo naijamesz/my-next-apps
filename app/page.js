a8bdb70adb2a183676045124428b0b8be97da87a
naijamesz <kodlikejamesz@hotmail.com>
2024-02-19 19:04:45 +0700

    tailwindcss section-0-to09.

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div className='flex items-center min-h-screen mx-auto justify-evenly'>
+      <div className='w-32 p-6 bg-green-400'>product 1</div>
+      <div className='w-32 p-6 bg-green-400'>product 2</div>
+      <div className='w-32 p-6 bg-green-400'>product 3</div>
+      <div className='w-32 p-6 bg-green-400'>product 4</div>
+    </div>
+  );
+}
